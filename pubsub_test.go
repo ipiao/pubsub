3953232540b851e6e500465d218326c4d3f9d584
ipiao <yukk046@zhixubao.com>
2017-10-27 14:38:53 +0800

    same as pre

--- pubsub_test.go
+++ pubsub_test.go
@@ -9,39 +9,39 @@ import (
 
 func TestPubSub(t *testing.T) {
 	ps := New(1)
-	handles1 := []handle{
-		handle{topic: "1", asny: false, handler: func(interface{}) interface{} {
+	handles1 := []Handle{
+		Handle{topic: "1", asny: false, handler: func(interface{}) interface{} {
 			log.Println("topic 1 from handles1")
 			return nil
 		}},
-		handle{topic: "2", asny: false, handler: func(interface{}) interface{} {
+		Handle{topic: "2", asny: false, handler: func(interface{}) interface{} {
 			log.Println("topic 2 from handles1")
 			return nil
 		}},
-		handle{topic: "3", asny: true, handler: func(interface{}) interface{} {
+		Handle{topic: "3", asny: true, handler: func(interface{}) interface{} {
 			log.Println("topic 3 from handles1")
 			return errors.New("hello1")
 		}},
-		handle{topic: "4", asny: false, handler: func(interface{}) interface{} {
+		Handle{topic: "4", asny: false, handler: func(interface{}) interface{} {
 			log.Println("topic 4 from handles1")
 			return errors.New("hello2")
 		}},
 	}
 
-	handles2 := []handle{
-		handle{topic: "1", asny: false, handler: func(interface{}) interface{} {
+	handles2 := []Handle{
+		Handle{topic: "1", asny: false, handler: func(interface{}) interface{} {
 			log.Println("topic 1 from handles2")
 			return nil
 		}},
-		handle{topic: "2", asny: false, handler: func(interface{}) interface{} {
+		Handle{topic: "2", asny: false, handler: func(interface{}) interface{} {
 			log.Println("topic 2 from handles2")
 			return nil
 		}},
-		handle{topic: "3", asny: false, handler: func(interface{}) interface{} {
+		Handle{topic: "3", asny: false, handler: func(interface{}) interface{} {
 			log.Println("topic 3 from handles2")
 			return errors.New("hello1")
 		}},
-		handle{topic: "4", asny: false, handler: func(msg interface{}) interface{} {
+		Handle{topic: "4", asny: false, handler: func(msg interface{}) interface{} {
 			mes := msg.(Message)
 			log.Println(mes)
 			log.Println("topic 4 from handles2")
